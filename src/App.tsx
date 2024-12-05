b19bdf264780d899d9d1afcd3ca2619c37d06712
Vansh Dhawan <vanshdhawan2505@gmail.com>
2024-12-06 00:16:24 +0530

    Firebase Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom"
+import {  Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import UserForm from "./pages/form"
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/form" element={<UserForm />} /> */}
       </Routes>
     </>
   )
